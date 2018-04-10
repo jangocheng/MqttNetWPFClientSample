4e39e14fc1060741622e437b3452e40271eb2893
Ralph Shillington <ralph@rgsc.net>
2018-04-10 07:28:18 -0400

    Initial Commit

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.ManagedClient;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            mqttClient = new MqttFactory().CreateMqttClient();
+            mqttClient.ApplicationMessageReceived += MqttClient_ApplicationMessageReceived;
+            mqttClient.Connected += MqttClient_Connected;
+            mqttClient.Disconnected += MqttClient_Disconnected;
+            
+        }
+
+        private IMqttClientOptions options;
+        private IMqttClient mqttClient;
+
+        private async void Button_Connect(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                options = new MqttClientOptionsBuilder()
+                            .WithCommunicationTimeout(TimeSpan.FromSeconds(10))     // Default value
+                            .WithKeepAlivePeriod(TimeSpan.FromSeconds(15))          // Default Value
+                            .WithWebSocketServer(mqttAddress.Text)
+                            .Build();
+
+                var result = await mqttClient.ConnectAsync(options);
+                DisplayMessage(mqttClient.IsConnected ? "Apparently the client is connected" : "WHAT the client isn't connected?");
+            }
+            catch(Exception ex)
+            {
+                DisplayMessage($"EXCEPTION IN Button_Connect {ex.Message}");
+            }
+            
+        }
+
+        private void DisplayMessage(string message)
+        {
+            Dispatcher.BeginInvoke((Action)(() => { messages.Items.Add($"{DateTime.Now}\t{message}"); }));
+        }
+        private async void MqttClient_Disconnected(object sender, MqttClientDisconnectedEventArgs e)
+        {
+            DisplayMessage("Disconnected");
+            await Task.CompletedTask;
+        }
+
+        private async void MqttClient_Connected(object sender, MqttClientConnectedEventArgs e)
+        {
+            DisplayMessage("Connected");
+            await Task.CompletedTask;
+        }
+
+        private async void MqttClient_ApplicationMessageReceived(object sender, MqttApplicationMessageReceivedEventArgs e)
+        {
+
+            try
+            {
+                var message = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+                DisplayMessage(message);
+                await Task.CompletedTask;
+            }
+            catch(Exception ex)
+            {
+                DisplayMessage(ex.Message);
+            }
+        }
+ 
+        private async void Button_Disconnect(object sender, RoutedEventArgs e)
+        {
+            await mqttClient.DisconnectAsync();
+        }
+        private async void Button_Subscribe(object sender, RoutedEventArgs e)
+        {
+            DisplayMessage(mqttClient.IsConnected.ToString());
+            try
+            {
+                if (mqttClient.IsConnected)
+                {
+                    DisplayMessage("Subscribing");
+                    var result = await mqttClient.SubscribeAsync(new TopicFilterBuilder().WithTopic(Topic.Text).Build());
+                    DisplayMessage($"Subscribe result: {result.SingleOrDefault()?.ReturnCode.ToString()}");
+                }
+                else
+                    DisplayMessage("Client is not connected");
+            }
+            catch(Exception ex)
+            {
+                DisplayMessage($"EXCEPTION IN Button_Subscribe.  {ex.Message}");
+            }
+        }
+
+        private async void Button_Unsubscribe(object sender, RoutedEventArgs e)
+        {
+            await mqttClient.UnsubscribeAsync(Topic.Text);
+        }
+
+        private async void Button_Publish(object sender, RoutedEventArgs e)
+        {
+            DisplayMessage($"Publishing message {message.Text} to topic {Topic.Text}");
+            await mqttClient.PublishAsync(new MqttApplicationMessageBuilder().WithTopic(Topic.Text).WithPayload(message.Text).Build());
+        }
+
+        private void Button_MessageClear(object sender, RoutedEventArgs e)
+        {
+            messages.Items.Clear();
+        }
+    }
+}
